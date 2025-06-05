0a1924f83c9cbbf5953c1334fa9d8293fb75df79
pde0814 <dongeon8943@gmail.com>
2025-06-05 16:05:16 +0900

    chore: 프로젝트 생성 및 폴더 구조 설정

---
+++ LeaveGo/LeaveGo/Feature/Home/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LeaveGo
+//
+//  Created by 박동언 on 6/5/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
